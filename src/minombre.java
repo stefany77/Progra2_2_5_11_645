449eecc5ef84f703707cd1f7492ffa599a9f3fde
Stefany <stefany_1100@hotmail.com>
2011-10-07 08:57:17 -0600

    este es mi primera modificacion

---
+++ src/minombre.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Stefany
+ */
+public class minombre {
+    public static void main (String arg[]){
+    System.out.print("Stefany Garcia");
+    }
+}
